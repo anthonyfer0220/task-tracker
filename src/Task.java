2311bb2adc0901f982f7b0043876f49bd12004ed
Tony Fernandez <tonyfg0220@gmail.com>
2024-11-30 21:12:11 -0500

    Create Task, TaskManager, and TaskTracker classes, and json file

---
+++ src/Task.java
@@ -0,0 +1,42 @@
+import java.time.LocalDateTime;
+
+public class Task {
+    private int id;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public Task(int id, String description) {
+        this.id = id;
+        this.description = description;
+        this.status = "todo";
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    // Getters and Setters
+    public int getId() { return id; }
+
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    public String getStatus() { return status; }
+
+    public void setStatus(String status) {
+        this.status = status;
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+
+    public LocalDateTime getUpdatedAt() { return updatedAt; }
+
+    @Override
+    public String toString() {
+        return "Task{id=" + id + ", description=" + description +
+                ", status=" + status + '\'' + ", createdAt=" + createdAt +
+                ",updatedAt=" + updatedAt + '}';
+    }
+}
