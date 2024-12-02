a5971597020f4a858fa0bf49ce475c4dc427f1c9
Anthony Fernandez <anthonyfer0220@gmail.com>
2024-12-01 20:21:12 -0500

    Comment out Date

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,18 +1,18 @@
-import java.time.LocalDateTime;
-
 public class Task {
     private int id;
     private String description;
     private String status;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+//    private LocalDateTime createdAt;
+//    private LocalDateTime updatedAt;
 
     public Task(int id, String description) {
         this.id = id;
         this.description = description;
         this.status = "todo";
+/*
         this.createdAt = LocalDateTime.now();
         this.updatedAt = LocalDateTime.now();
+*/
     }
 
     // Getters and Setters
@@ -26,17 +26,16 @@ public class Task {
 
     public void setStatus(String status) {
         this.status = status;
-        this.updatedAt = LocalDateTime.now();
+//        this.updatedAt = LocalDateTime.now();
     }
 
-    public LocalDateTime getCreatedAt() { return createdAt; }
-
-    public LocalDateTime getUpdatedAt() { return updatedAt; }
+//    public LocalDateTime getCreatedAt() { return createdAt; }
+//
+//    public LocalDateTime getUpdatedAt() { return updatedAt; }
 
     @Override
     public String toString() {
-        return "Task{id=" + id + ", description=" + description +
-                ", status=" + status + '\'' + ", createdAt=" + createdAt +
-                ",updatedAt=" + updatedAt + '}';
+        return "Task { id=" + id + ", description=" + description +
+                ", status=" + status + " }";
     }
 }
